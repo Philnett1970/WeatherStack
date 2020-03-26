007718026878a27f260b5d0cecd2839d6d7fcaf5
Philip Nettles <philnett@outlook.com>
2020-03-26 09:07:40 -0600

    Add project files.

---
+++ WeatherStack/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+using static WeatherStack.WeatherForecast;
+
+namespace WeatherStack
+{
+    
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            // get the weather when app loads
+            GetWeather();
+        }
+
+        private async void GetWeather()
+        {
+            // set the city name
+            WeatherObject weather = await WeatherForecast.GetCurrentWeatherFromCityName("Parachute");
+            //variable to hold image string
+            var strimg = weather.current.weather_icons[0].ToString();
+            // create bitmapimage object
+            BitmapImage img = new BitmapImage(new Uri(strimg, UriKind.Absolute));
+
+            try
+            {
+                LocationName.Text = "Location: " + weather.location.name;
+                LocationRegion.Text ="State/Region:" + weather.location.region;
+                CurrentTemp.Text = "Current Temp:" + weather.current.temperature.ToString();
+                IMG.Source = img;
+
+            }
+            catch (Exception)
+            {
+                LocationRegion.Text = "unable to get the weather";
+            }
+        }
+
+    }
+}
---
+++ WeatherStack/WeatherForecast.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherStack
+{
+    public class WeatherForecast
+    {
+
+
+        public static async Task<WeatherObject> GetCurrentWeatherFromCityName(string str)
+        {
+            var http = new HttpClient();
+            // get you free key from https://weatherstack.com/
+
+            var url = String.Format("http://api.weatherstack.com/current?access_key=YOURKEYHERE&query={0}&units=f", str);
+            var response = await http.GetAsync(url);
+            var result = await response.Content.ReadAsStringAsync();
+
+            var serializer = new DataContractJsonSerializer(typeof(WeatherObject));
+
+            var memorystream = new MemoryStream(Encoding.UTF8.GetBytes(result));
+            var data = (WeatherObject)serializer.ReadObject(memorystream);
+
+            return data;
+        }
+                     
+        public class Request
+        {
+            public string type { get; set; }
+            public string query { get; set; }
+            public string language { get; set; }
+            public string unit { get; set; }
+        }
+
+        public class Location
+        {
+            public string name { get; set; }
+            public string country { get; set; }
+            public string region { get; set; }
+            public string lat { get; set; }
+            public string lon { get; set; }
+            public string timezone_id { get; set; }
+            public string localtime { get; set; }
+            public int localtime_epoch { get; set; }
+            public string utc_offset { get; set; }
+        }
+
+        public class Current
+        {
+            public string observation_time { get; set; }
+            public int temperature { get; set; }
+            public int weather_code { get; set; }
+            public List<string> weather_icons { get; set; }
+            public List<string> weather_descriptions { get; set; }
+            public int wind_speed { get; set; }
+            public int wind_degree { get; set; }
+            public string wind_dir { get; set; }
+            public int pressure { get; set; }
+            public int precip { get; set; }
+            public int humidity { get; set; }
+            public int cloudcover { get; set; }
+            public int feelslike { get; set; }
+            public int uv_index { get; set; }
+            public int visibility { get; set; }
+        }
+
+        public class WeatherObject
+        {
+            public Request request { get; set; }
+            public Location location { get; set; }
+            public Current current { get; set; }
+        }
+    }
+
+    
+}
